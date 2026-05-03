f33019da7c09fdcc0070eedd8b8c1015e01b9851
MercuNC <jorgemorenof98@gmail.com>
2026-05-03 16:40:20 -0400

    Cambios curiosos

--- P_PP.py
+++ P_PP.py
@@ -1,61 +1,43 @@
-membresia = 20000
-botella_d = 6000
-total = 0
-d = ''
-de = ''
+VALOR_MEMBRESIA = 20000
+VALOR_BOTELLA = 6000
+
 try:
+    # Entradas pedidas al usuario
     dias = int(input('Ingrese la cantidad de días asistidos: '))
     a_mayor = input('Eres adulto mayor(S/N): ').lower()
+    # Validación datos válidos
+    if dias <= 0:
+        print('El número tiene que ser mayor a 0.')
+        exit()
+    if a_mayor not in ('s','si','sí','n','no'):
+        print('Ingrese una "S" o "N"')
+        exit()
+
 except ValueError as e:
     print(f'Error: {e}')
 else:
-    print('')
-    print('Descuentos:')
-    if dias >= 20 and (a_mayor == 's' or a_mayor== 'si' or a_mayor=='sí'):
-        print('Tendrá un "30%" de descuento en membresía.')
-        total = (membresia*30) /100
-        membresia -= total
-        total = 0
-        d = '30%'
-    elif dias >= 20 and (a_mayor == 'n' or a_mayor== 'no'):
-        print('Tendrá un "20%" de descuento en membresía.')
-        total = (membresia*20)/100
-        membresia -= total
-        total = 0
-        d = '20%'
-    elif (10<= dias < 20) and (a_mayor == 's' or a_mayor== 'si' or a_mayor == 'sí'):
-        print('Tendrá un "18%" de descuento en membresía.')
-        total = (membresia*18)/100
-        membresia -= total
-        total = 0
-        d = '18%'
-    elif (10<= dias < 20) and (a_mayor == 'n' or a_mayor== 'no'):
-        print('Tendrá un "10%" de descuento en membresía.')
-        total = (membresia*10)/100
-        membresia -= total
-        total = 0
-        d = '10%'
-    else:
-        print('No tendrá descuentos')
-        d = '0%'
-    print(f'Tendrá un "{d}" de descuento en membresía.')
+    es_mayor = a_mayor in ('s','si','sí')
 
-    if dias >= 15 and (a_mayor == 's' or a_mayor== 'si' or a_mayor=='sí'):
-        print('Tendra un "17%" de descuento en la "Botella Deportiva"')
-        total = (botella_d*17)/100
-        botella_d -= total
-        de = '17%'
-    elif (a_mayor == 's' or a_mayor== 'si' or a_mayor=='sí'):
-        print('Tendra un "12%" de descuento en "Botella Deportiva"')
-        total= (botella_d*12)/100
-        botella_d -= total
-        de = '12%'
+    #descuentos membresía dias >= 20 y es mayor 30, si no 20, 10<=dias < 20  si es mayor 18, si no 10, si ninguna 0
+    if dias >= 20:
+        descuento = 0.3 if es_mayor else 0.2
+    elif 10 <= dias < 20:
+        descuento = 0.18 if es_mayor else 0.1
     else:
-        print('No tendrás descuentos')
-        de='0%'
+        descuento = 0
+
+    #descuento botella si dias >= 15 si es mayor 17, si no 0(USO DE TERCIARIOS)
+    if es_mayor:
+        descuento_botella = 0.17 if dias >= 15 else 0.12
+    else:
+        descuento_botella = 0
+
+    descuento_membresiafin = VALOR_MEMBRESIA-(VALOR_MEMBRESIA*descuento)
+    descuento_botellafinal = VALOR_BOTELLA-(VALOR_BOTELLA*descuento_botella)
+    total = descuento_botellafinal+descuento_membresiafin
     
     print('')
     print('Detalles:')
-    print(f'Membresía: $20.000 - {d} = {membresia:0.0f}')
-    print(f'Botella: $6000 - {de} = {botella_d:0.0f}')
-    print(f'Total a pagar: ${membresia+botella_d:0.0f}')
+    print(f'Membresía: ${VALOR_MEMBRESIA} - {descuento*100} = {descuento_membresiafin:.0f}')
+    print(f'Botella: ${VALOR_BOTELLA} - {descuento_botella*100} = {descuento_botellafinal:.0f}')
+    print(f'Total a pagar: ${total:.0f}')
