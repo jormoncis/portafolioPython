0e2f857bedb203c7f11edfaf5a924e46829c14a1
MercuNC <jorgemorenof98@gmail.com>
2026-05-12 22:01:17 -0400

    Quitar el try. por uso invalido

--- Random´s/verificador_correo.py
+++ Random´s/verificador_correo.py
@@ -2,37 +2,34 @@ DOMINIO_VALIDO = '@duocuc.cl'
 nom = ''
 dom = ''
 acceso = False
-try:
-    correo = input('Ingrese su correo: ')
-    for i in correo:
-        if i == '@': acceso = True
 
-        if not acceso:
-            nom += i
-        else:
-            dom += i
+correo = input('Ingrese su correo: ')
+for i in correo:
+    if i == '@': acceso = True
 
-    contar_pun = 0
-    correo_NV = True
-    for i in nom:
-        if i == '.':
-                contar_pun += 1
-        if i in ('1234567890'):
-            correo_NV = False 
-    
-    if dom != DOMINIO_VALIDO:
-        print('Tiene que tener un dominio válido.')
-
-    elif not (10 <= len(nom) <= 11):
-        print('El correo debe tener entre 10 y 11 caracteres.')
-
-    elif contar_pun >= 2:
-        print('El correo solo puede contener un punto.')
-
-    elif not correo_NV:
-        print('El correo no puede contener dígitos.')
+    if not acceso:
+        nom += i
     else:
-        print('Correo correcto!')
+        dom += i
 
-except TypeError as e:
-    print(f'Error: {e}')
+contar_pun = 0
+correo_NV = True
+for i in nom:
+    if i == '.':
+            contar_pun += 1
+    if i in ('1234567890'):
+        correo_NV = False 
+
+if dom != DOMINIO_VALIDO:
+    print('Tiene que tener un dominio válido.')
+
+elif not (10 <= len(nom) <= 11):
+    print('El correo debe tener entre 10 y 11 caracteres.')
+
+elif contar_pun >= 2:
+    print('El correo solo puede contener un punto.')
+
+elif not correo_NV:
+    print('El correo no puede contener dígitos.')
+else:
+    print('Correo correcto!')
