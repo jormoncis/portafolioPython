34493e7b392e3a49cfbbb463316c98c41057a839
MercuNC <jorgemorenof98@gmail.com>
2026-05-12 21:47:53 -0400

    Detalles arrglados

--- Random´s/verificador_correo.py
+++ Random´s/verificador_correo.py
@@ -5,7 +5,7 @@ acceso = False
 try:
     correo = input('Ingrese su correo: ')
     for i in correo:
-        if i in '@': acceso = True
+        if i == '@': acceso = True
 
         if not acceso:
             nom += i
@@ -13,24 +13,24 @@ try:
             dom += i
 
     contar_pun = 0
+    correo_NV = True
     for i in nom:
         if i == '.':
                 contar_pun += 1
         if i in ('1234567890'):
             correo_NV = False 
     
-    if not (10 <= len(nom) <= 11 and dom in DOMINIO_VALIDO):
-        if not(dom in DOMINIO_VALIDO):
-            print('Tiene que tener un dominio válido.')
-            exit()
-        elif 10 <= len(nom) <= 11:
-            print('El correo tiene que tener 10 caracteres.')
-    if contar_pun >= 2:
+    if dom != DOMINIO_VALIDO:
+        print('Tiene que tener un dominio válido.')
+
+    elif not (10 <= len(nom) <= 11):
+        print('El correo debe tener entre 10 y 11 caracteres.')
+
+    elif contar_pun >= 2:
         print('El correo solo puede contener un punto.')
-        exit()
-    if correo_NV== False:
-        print('El correo no puede contener ningun digito en el.')
-        exit()
+
+    elif not correo_NV:
+        print('El correo no puede contener dígitos.')
     else:
         print('Correo correcto!')
 
