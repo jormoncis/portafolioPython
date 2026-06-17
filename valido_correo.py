d8d1561ff971f1e8d102fbcedec70df464114ce9
MercuNC <jorgemorenof98@gmail.com>
2026-06-17 19:22:38 -0400

    nuevos cambios y mejoras

--- valido_correo.py
+++ valido_correo.py
@@ -1,17 +1,27 @@
-ruts = ['19992116-9','20.056.223-8','8254336-k']
+ruts = ['20.056.223-8','8.254.336-k']
 
 def mensaje():
+    '''
+    Uso solo para mensaje de input'''
     return input('Ingrese su rut(Para salir digite x): ')
 
 def ingreso(rut):
+    '''
+    Aúnque similar a busca_rut este lo compara con el formato correspondiente si lo encuentra manda el mensaje que se encuentra,
+    si no lo agrega.
+    '''
     if rut in ruts:
         print('El rut procesado ya existe.')
         busca_rut(mensaje())
     else:
         ruts.append(rut)
+        print(ruts)
         print('Rut ingresado correctamente.')
 
 def formateo(rut_limpio, dv):
+    '''
+    Analiza la parte númerica y la clasifica, La ordena en el formato correcto.
+    '''
     numero = rut_limpio
     
     if len(numero) == 8:
@@ -20,43 +30,39 @@ def formateo(rut_limpio, dv):
     elif len(numero) == 7:
         formato_rut = numero[0:1] + '.'+ numero[1:4]+'.'+numero[4:7]+'-'+dv
 
-    elif len(numero) == 10:
-        formato_rut = rut_limpio + '-' + dv
-
     return ingreso(formato_rut)
 
 def limpieza(rut):
+    ''' 
+    Recibe un rut formato ejemplo(11111111-1,11.111111-1, 11.111.111-1)
+    Lo limpia y separa en números sin punto antes del "-" y el digito verificador.
+    SOLO limpieza
+    resultado => numero = 11111111    dvv = 1
+    '''
     numero = rut[:-2]
     dvv = rut[-1]
-    nuevo_elemento = ''
-    
-    for elemento in numero:
-        if elemento != '.':
-            nuevo_elemento += elemento
 
+    nuevo_elemento = numero.replace('.', '')
     rut_correcto = nuevo_elemento.isdigit()
 
-    print(nuevo_elemento)
     if rut_correcto:
         return formateo(nuevo_elemento, dvv)
     else:
         busca_rut(mensaje())
 
 def busca_rut(rut_buscar):
-    if rut_buscar in 'xX':
-        exit()
+    '''
+    Busca si lo digitado esta dentro del registro actual de ruts, si lo encuentra manda el mensaje si no continua
+    '''
     if rut_buscar in ruts:
         print('El rut ingresado ya existe.')
         busca_rut(mensaje())
     else:
         limpieza(rut_buscar)
 
-def valido(rut):
-    if rut in ruts:
-        print('El rut ingresado ya existe.')
-        busca_rut(mensaje())
-    else:
-        banderilla = False
 
-banderilla = True
-busca_rut(mensaje())
+
+rut_ingreso = mensaje()
+if rut_ingreso.lower() in 'x':
+    exit()
+busca_rut(rut_ingreso)
